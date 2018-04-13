35a2841089c91d06ff00cf0160a3925792ffcab5
Jeff PinterParsons <jpinterp@gmail.com>
2018-04-12 23:24:35 -0400

    Use factory for obtaining data store interface

--- src/main/java/com/jpp/controllers/EventsController.java
+++ src/main/java/com/jpp/controllers/EventsController.java
@@ -1,16 +1,26 @@
 package com.jpp.controllers;
 
+import com.jpp.DataStoreFactory;
+import com.jpp.IDataStore;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/events")
 public class EventsController
 {
+    private IDataStore ds;
+
+    public EventsController()
+    {
+        ds = DataStoreFactory.getDataStore();
+    }
+
     @RequestMapping(method=RequestMethod.GET, produces={"application/JSON"})
     @ResponseBody
     String getEvents()
     {
-        return "{ \"greeting\": \"Hello, World!\"}";
+        String jsonData = ds.GetEventNames();
+        return jsonData;
     }
 
 }
